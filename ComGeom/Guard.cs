1625d583c59d6cb6f55b38c32d78ec44d1b53b30
Hrenaki <shikhovwtr@mail.ru>
2022-05-11 20:19:20 +0700

    Added Guard

---
+++ ComGeom/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComGeom
+{
+    internal static class Guard
+    {
+        public static void AgainstNull(object obj, string name)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(name);
+        }
+    }
+}
