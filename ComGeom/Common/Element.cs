b59d956c8ee3c7f2186801824779672afc711902
Hrenaki <shikhovwtr@mail.ru>
2022-05-11 20:21:04 +0700

    Some additions to Element

--- ComGeom/Common/Element.cs
+++ ComGeom/Common/Element.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace ComGeom
 {
@@ -8,6 +9,15 @@ namespace ComGeom
     {
         Triangle,
         Tetrahedron
+    }
+
+    internal static class ElementInfo
+    {
+        public static IReadOnlyDictionary<ElementType, int> ElementIndexCount = new ReadOnlyDictionary<ElementType, int>(new Dictionary<ElementType, int>()
+        { 
+            {ElementType.Triangle, 3 }, 
+            {ElementType.Tetrahedron, 4} 
+        });
     }
 
     public struct Element
@@ -18,14 +28,32 @@ namespace ComGeom
 
         public Element(ElementType type, int materialNumber, int[] indices)
         {
+            Guard.AgainstNull(indices, nameof(indices));
+
             Type = type;
             Indices = indices;
             MaterialNumber = materialNumber;
+
+            ValidateIndices();
+        }
+
+        private void ValidateIndices()
+        {
+            if (Indices == null)
+                return;
+
+            if(Indices.Any(index => index < 0))
+                throw new ArgumentException("Index can't be negative", nameof(Indices));
         }
 
         public Element CopyWithNewIndices(int[] newIndices)
         {
             return new Element(Type, MaterialNumber, newIndices);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", Type.ToString(), MaterialNumber.ToString(), string.Join(" ", Indices));
         }
     }
 }
