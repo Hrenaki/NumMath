f8bd5eb90ab0fd098f1e96f8588fdadc821f1632
Hrenaki <shikhovwtr@mail.ru>
2022-05-12 02:48:08 +0700

    Added 'AddRange' extension to Dictionary

---
+++ ComGeom/Common/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComGeom.Common
+{
+    internal static class Extensions
+    {
+        public static void AddRange<T, V>(this IDictionary<T, V> target, IEnumerable<KeyValuePair<T, V>> source) where T : notnull
+        {
+            foreach (var pair in source)
+                target.Add(pair.Key, pair.Value);
+        }
+    }
+}
